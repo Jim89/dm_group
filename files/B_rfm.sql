25590cb3ad7351d615184e6b79c6b4e6f734e3a9
Jim Leach <jimmy22theave@gmail.com>
2016-05-12 10:22:10 +0100

    Add rfm SQL calc file to repo

---
+++ files/B_rfm.sql
@@ -0,0 +1,29 @@
+﻿/* Drop tables if they exist to prevent errors *********/
+drop table if exists r_scores;
+
+/* Create r-scores */
+select
+	 *
+	,ntile(5) OVER(order by age) as r
+into r_scores	
+from
+(
+	select
+		 cust_id
+		,most_recent
+		,(cast('2008-01-01' as date) - most_recent) as age
+	from
+	(
+		select
+			 cust_id
+			,max(order_date) as most_recent
+		from
+			lines
+		group by
+			cust_id
+	)_	
+)_
+
+/*
+select * from r_scores
+*/	
