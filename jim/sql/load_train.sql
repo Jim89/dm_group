d07e88b4dd46c22853a8876f22cb4ba6a5c0142a
Jim Leach <jimmy22theave@gmail.com>
2016-06-01 08:18:24 +0100

    Add training data to PG database and create a sample

---
+++ jim/sql/load_train.sql
@@ -0,0 +1,32 @@
+﻿drop table if exists train;
+create table train (
+date_time 	text
+,site_name 	int
+,posa_continent 	int
+,user_location_country 	int
+,user_location_region 	int
+,user_location_city 	int
+,orig_destination_distance 	float
+,user_id 	int
+,is_mobile 	int
+,is_package 	int
+,channel 	int
+,srch_ci 	text
+,srch_co 	text
+,srch_adults_cnt int
+,srch_children_cnt int
+,srch_rm_cnt 	int
+,srch_destination_id 	int
+,srch_destination_type_id 	int
+,hotel_continent 	int
+,hotel_country 	int
+,hotel_market 	int
+,is_booking 	int
+,cnt 	bigint
+,hotel_cluster 	int
+)
+
+COPY train
+FROM
+	'C:\Users\Jleach1\Documents\icl\dm\data\train.csv'
+WITH CSV HEADER
---
+++ jim/sql/train_sample.sql
@@ -0,0 +1,12 @@
+﻿drop table if exists train_sample;
+select * 
+into train_sample
+from 
+	train 
+where random() < 0.01 
+limit 0.1*37670293;
+
+/*
+select * from train_sample
+select count(*) from train_sample;
+*/
