f665df9360f5073b06bb1dfc6e53f5d170e01c31
Jim Leach <jimmy22theave@gmail.com>
2016-06-01 11:45:51 +0100

    Correct field name order based on the raw file

--- jim/sql/load_train.sql
+++ jim/sql/load_train.sql
@@ -1,29 +1,29 @@
 ﻿drop table if exists train;
 create table train (
-date_time 	text
-,site_name 	int
-,posa_continent 	int
-,user_location_country 	int
-,user_location_region 	int
-,user_location_city 	int
-,orig_destination_distance 	float
-,user_id 	int
-,is_mobile 	int
-,is_package 	int
-,channel 	int
-,srch_ci 	text
-,srch_co 	text
+date_time text
+,site_name int
+,posa_continent int
+,user_location_country int
+,user_location_region int
+,user_location_city int
+,orig_destination_distance float
+,user_id int
+,is_mobile int
+,is_package int
+,channel int
+,srch_ci text
+,srch_co text
 ,srch_adults_cnt int
-,srch_children_cnt int
-,srch_rm_cnt 	int
-,srch_destination_id 	int
-,srch_destination_type_id 	int
-,hotel_continent 	int
-,hotel_country 	int
-,hotel_market 	int
-,is_booking 	int
-,cnt 	bigint
-,hotel_cluster 	int
+,srch_children_cnt int 
+,srch_rm_cnt int
+,srch_destination_id int
+,srch_destination_type_id int
+,is_booking int
+,cnt int
+,hotel_continent int
+,hotel_country int
+,hotel_market int
+,hotel_cluster int
 )
 
 COPY train
