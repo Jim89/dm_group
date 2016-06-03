d13a83c9ab10255080bc65b8d615a22323d0eb1d
Jim Leach <jimmy22theave@gmail.com>
2016-06-03 10:05:09 +0100

    Add code to create and load new training data

---
+++ jim/sql/train_new_sample.sql
@@ -0,0 +1,11 @@
+﻿drop table if exists train_new_sample;
+select *
+into train_new_sample
+from
+	train_new
+where srch_id <= 100
+
+/*
+select count(*) from train_new_sample
+select * from train_new_sample limit 100
+*/
